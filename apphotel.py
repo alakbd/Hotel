d57c58b5640125ced83008afd815dd9c89e0b4e2
AL <alakbd2009@gmail.com>
2021-05-30 00:08:04 +0000

    update apphotel.py

--- apphotel.py
+++ apphotel.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 import json
+import datetime
 mysql = MySQL()
 app = Flask(__name__)
 CORS(app)
@@ -14,7 +15,7 @@ app.config['MYSQL_DB'] = 'hotel'
 app.config['MYSQL_HOST'] = 'localhost' #Use IP
 mysql.init_app(app)
 
-@app.route("/addCustomer") #Add Customer
+@app.route("/addCustomer",methods=["POST"]) #Add Customer
 def addCustomer():
   custName = request.args.get('name')
   email = request.args.get('email')
@@ -72,5 +73,82 @@ def bookingPage(): # Name of the method
     mimetype='application/json'
   )
   return ret #Return the data in a string format
+
+
+@app.route("/AvailableRooms") #Default - Show Data
+def AvailableRooms(): # Name of the method
+  no_of_bed = request.args.get('rooms')
+  cur = mysql.connection.cursor()  #create a connection to the SQL instance
+  cur.execute("SELECT * FROM roomTbl where bed="+no_of_bed+" and bookingStatus=0;") # execute an SQL statment
+  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
+  Results=[]
+  for row in rv: #Format the Output Results and add to return string////////windowDir, bed,accessory,imageUrl,bookingStatus, rlevel
+    Result={}
+    Result['windowDir']=row[0].replace('\n',' ')
+    Result['bed']=row[1]
+    Result['accessory']=row[2]
+    Result['imageUrl']=row[3]
+    Result['bookingStatus']=row[4]
+    Result['rlevel']=row[5]
+    Result['price'] = row[6]
+    Result['roomID'] = row[7]
+    Results.append(Result)
+  response={'Results':Results, 'count':len(Results)}
+  ret=app.response_class(
+    response=json.dumps(response),
+    status=200,
+    mimetype='application/json'
+  )
+  return ret #Return the data in a string format
+
+
+@app.route("/bookingEntry",methods=["POST"]) #Add Customer
+def bookingEntry():
+  custName = request.args.get('name')
+  phone = request.args.get('phone')
+  address = request.args.get('address')
+  idtype = request.args.get('idtype')
+  idnumber = request.args.get('idnumber')
+  email = request.args.get('email')
+  sex = request.args.get('sex')
+  comment = request.args.get('comment')
+  cur = mysql.connection.cursor() #create a connection to the SQL instance
+  s='''INSERT INTO custTbl(custName,phone,address,idtype,idnumber,email,sex,comment) VALUES('{}','{}','{}','{}','{}','{}','{}','{}');'''.format(custName,phone,address,idtype,idnumber,email,sex,comment)
+  cur.execute(s)
+  mysql.connection.commit()
+
+  return '{"Result":"Success"}'
+
+@app.route("/bookingEntryV",methods=["POST"]) #Add Customer
+def bookingEntryV():
+  custName = request.args.get('name')
+  phone = request.args.get('phone')
+  address = request.args.get('address')
+  idtype = request.args.get('idtype')
+  idnumber = request.args.get('idnumber')
+  email = request.args.get('email')
+  sex = request.args.get('sex')
+  comment = request.args.get('comment')
+  checkin_date = request.args.get('checkin_date')
+  checkout_date = request.args.get('checkout_date')
+  roomID = request.args.get('roomID')
+  cur = mysql.connection.cursor() #create a connection to the SQL instance
+  s='''INSERT INTO custTbl(custName,phone,address,idtype,idnumber,email,sex,comment) VALUES('{}','{}','{}','{}','{}','{}','{}','{}');'''.format(custName,phone,address,idtype,idnumber,email,sex,comment)
+  cur.execute(s)
+  mysql.connection.commit()
+  cur2 = mysql.connection.cursor()
+  cur2.execute('''select MAX(custID) From custTbl''') # execute an SQL statment
+  rv = cur2.fetchall() #Retreive all rows returend by the SQL statment
+  Result=0
+  for row in rv: #Format the Output Results and add to return string
+    Result=row[0]
+  cur3 = mysql.connection.cursor()
+  s3='''insert into reservationTbl ( checkin_date, checkout_date, custID, roomID ) VALUES('{}','{}','{}','{}');'''.format(checkin_date, checkout_date, Result, roomID)
+  cur3.execute(s3)
+  mysql.connection.commit()
+  return '{"Result":"Success"}'
+
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
